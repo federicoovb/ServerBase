fb6b32270d32620cf69d0f920f2b64f7a0d697f7
Federico Viola Boros <74244173+federicoovb@users.noreply.github.com>
2021-06-18 15:23:23 +0200

    upload all

--- src/serverbase/ServerBase.java
+++ src/serverbase/ServerBase.java
@@ -5,10 +5,7 @@
  */
 package serverbase;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,6 +14,7 @@ import java.util.logging.Logger;
  *
  * @author violaboros.federico
  */
+
 public class ServerBase {
     private int port;
     private ServerSocket SS;
